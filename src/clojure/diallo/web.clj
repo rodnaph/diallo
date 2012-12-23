32ad47633169d24df7987da2e7b8d6b92c23775b
rod <rod@pu-gh.com>
2012-12-23 09:52:47 +0000

    tweaked routes

--- src/clojure/diallo/web.clj
+++ src/clojure/diallo/web.clj
@@ -23,38 +23,22 @@
     {:body (pr-str data)} 
     "application/edn"))
 
-;; Pages
-;; -----
-
-(defn- page-explore [req]
-  (html/explore-template))
-
-(defn- page-index [req]
-  (html/index-template (jenko/views)))
-
-(defn- page-view [view req]
-  (html/view-template view (jobs-for view)))
-
-;; API
-;; ---
-
-(defn- api-views [req]
-  (edn-response (jenko/views))) 
-
-(defn- api-jobs [view req]
-  (edn-response (jobs-for view)))
-
-;; Routes
-;; ------
-
 (defroutes app-routes
 
-  (GET "/" [] page-index)
-  (GET "/explore" [] page-explore)
-  (GET "/views/:view" [view] (partial page-view view))
+  (GET "/" []
+       (html/index-template (jenko/views)))
 
-  (GET "/api/views" [] api-views)
-  (GET "/api/views/:view" [view] (partial api-jobs view))
+  (GET "/explore" []
+       (html/explore-template))
+
+  (GET "/views/:view" [view]
+       (html/view-template view (jobs-for view)))
+
+  (GET "/api/views" []
+       (edn-response (jenko/views)))
+
+  (GET "/api/views/:view" [view]
+       (edn-response (jobs-for view)))
 
   (route/resources "/assets")
   (route/not-found "404..."))
